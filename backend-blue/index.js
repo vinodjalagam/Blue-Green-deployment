07784af08fc21ca3a47689931590d35e02a5d072
vinodjalagam <vinodjalagam477@gmail.com>
2024-07-29 23:11:36 +0530

    initial commit

---
+++ backend-blue/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+// Route: /sayHello
+app.get('/sayHello', (req, res) => {
+    res.json({ message: 'Hello vinodzsxcvbn Vinod Kumar Goud ' });
+});
+
+const PORT = process.env.PORT || 8085;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
