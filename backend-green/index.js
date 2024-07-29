97e3c877f198a8e2e3e25b7a41a56654fc3677b3
vinodjalagam <vinodjalagam477@gmail.com>
2024-07-30 00:49:59 +0530

    initial commit

--- backend-green/index.js
+++ backend-green/index.js
@@ -3,7 +3,7 @@ const app = express();
 
 // Route: /sayHello
 app.get('/sayHello', (req, res) => {
-    res.json({ message: 'Hello vinodzsxcvbn Vinod Kumar Goud ' });
+    res.json({ message: 'Hello  Vinod Kumar Goud ' });
 });
 
 const PORT = process.env.PORT || 8086;
