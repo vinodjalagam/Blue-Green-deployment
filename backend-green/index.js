a533ba07dbc59c1a0067005778e706b84ff98c2b
vinodjalagam <63002962+vinodjalagam@users.noreply.github.com>
2024-07-30 01:28:21 +0530

    Update index.js

--- backend-green/index.js
+++ backend-green/index.js
@@ -3,7 +3,7 @@ const app = express();
 
 // Route: /sayHello
 app.get('/sayHello', (req, res) => {
-    res.json({ message: 'Hello  Vinod Kumar Goud vinoenfibuvsbinxm ' });
+    res.json({ message: 'Hello  Vinod Kumar Goud vinod 767492270293udj ' });
 });
 
 const PORT = process.env.PORT || 8086;
