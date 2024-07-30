fcf3a03e887fb2a2dacf8ce6b22374cdcbef8c8c
vinodjalagam <63002962+vinodjalagam@users.noreply.github.com>
2024-07-30 11:59:36 +0530

    Update index.js

--- backend-blue/index.js
+++ backend-blue/index.js
@@ -6,7 +6,7 @@ app.get('/sayHello', (req, res) => {
     res.json({ message: 'Hello  vinod kumar WelCome Bacck' });
 });
 
-const PORT = process.env.PORT || 8085;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
