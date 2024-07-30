036c2cb1526c2a9a7f7c1511142f57d7b54c9a00
vinodjalagam <63002962+vinodjalagam@users.noreply.github.com>
2024-07-30 10:54:58 +0530

    Update index.js

--- backend-blue/index.js
+++ backend-blue/index.js
@@ -3,7 +3,7 @@ const app = express();
 
 // Route: /sayHello
 app.get('/sayHello', (req, res) => {
-    res.json({ message: 'Hello  Vinod Kumar Goud user mobile number is 7674922470' });
+    res.json({ message: 'Hello  User' });
 });
 
 const PORT = process.env.PORT || 8085;
